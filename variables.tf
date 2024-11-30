eb85253fbabd1d04913ca6172ef5a8182e60f078
Princy Kshirsagar <112242191+princyi@users.noreply.github.com>
2024-11-30 11:22:48 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# Define input variables
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the Subnet"
+  default     = "10.0.1.0/24"
+}
